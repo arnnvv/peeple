9d08227cbd5786256baac5bcc2f94ba4ab4fcb91
arnnvv <arnav.900@protonmail.com>
2024-09-30 21:09:08 +0530

    ps

--- mobile/app/chk/index.tsx
+++ mobile/app/chk/index.tsx
@@ -3,28 +3,30 @@ import { userExists } from "@/lib/checkUser";
 import { useUser } from "@clerk/clerk-expo";
 import { router } from "expo-router";
 import { useSetAtom } from "jotai";
+import { useEffect } from "react";
 
-export default async () => {
+export default () => {
   const { isLoaded, user, isSignedIn } = useUser();
   const setEmail = useSetAtom(emailAtom);
-  if (!isLoaded) {
-    console.log("not loaded");
-    return;
-  }
-  if (!isSignedIn) {
-    console.log("not signed In");
-    return;
-  }
-  const email = user?.emailAddresses[0].emailAddress;
-  console.log(user);
-  if (email) {
-    const exists = await userExists(email);
-    if (exists === true) {
-      router.replace("/(tabs)/homeScreen");
-      //chk if name exists
-    } else if (exists === false) {
-      setEmail(email);
-      router.replace("/(onboarding)/moreyoushare");
-    }
-  }
+
+  useEffect(() => {
+    (async () => {
+      if (!isLoaded) {
+        console.log("not loaded");
+        return;
+      }
+      if (!isSignedIn) {
+        console.log("not signed In");
+        return;
+      }
+      const email = user?.emailAddresses[0].emailAddress;
+      console.log(user);
+      if (email) {
+        setEmail(email);
+        const exists = await userExists(email);
+        if (exists === true) router.replace("/(tabs)/homeScreen");
+        else if (exists === false) router.replace("/(onboarding)/moreyoushare");
+      }
+    })();
+  }, []);
 };
